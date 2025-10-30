1e632e2948bfe8dede63a9e766184d42620e258c
raul <raul@192.168.1.134>
2025-10-30 12:52:43 +0200

    Added debug info build option

--- build.zig
+++ build.zig
@@ -18,22 +18,27 @@ pub fn build(b: *std.Build) void {
         .link_libcpp = true,
     });
 
-    { // Options
-        const log_diagnostics = b.option(
-            LogDiagnostics,
-            "log_diagnostics",
-            "Should the per function slang diagnostic text be automatically logged using std.log (default: only_for_null)",
-        ) orelse .only_for_null;
+    const log_diagnostics = b.option(
+        LogDiagnostics,
+        "log_diagnostics",
+        "Should the per function slang diagnostic text be automatically logged using std.log (default: only_for_null)",
+    ) orelse .only_for_null;
 
-        const options = b.addOptions();
-        options.addOption(LogDiagnostics, "log_diagnostics", log_diagnostics);
-        mod.addOptions("options", options);
-    }
+    const use_debug_info = b.option(
+        bool,
+        "debug_info",
+        "Use builds of slang that include debug info in debug modes (default: true)",
+    ) orelse true;
+
+    const options = b.addOptions();
+    options.addOption(LogDiagnostics, "log_diagnostics", log_diagnostics);
+    mod.addOptions("options", options);
+
     { // Dependencies
         const slang_dep_name = b.fmt("slang-{s}-{s}{s}", .{
             @tagName(target.result.os.tag),
             @tagName(target.result.cpu.arch),
-            if (is_debug) "-debug-info" else "",
+            if (is_debug and use_debug_info) "-debug-info" else "",
         });
         if (b.lazyDependency(slang_dep_name, .{})) |slang| {
             mod.addSystemIncludePath(slang.path("include"));
